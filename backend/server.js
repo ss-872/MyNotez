f93f610b93d5371aed4bcb734ac3bf372922cef3
ss-872 <101634846+ss-872@users.noreply.github.com>
2022-06-28 07:17:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ __dirname = path.resolve();
 
 if(process.env.NODE_ENV === 'production'){
     console.log("hi")
-    app.use(express.static(path.join(__dirname,'/frontend/build')));
+    app.use(express.static(path.join(__dirname,'/ui/build')));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+        res.sendFile(path.resolve(__dirname,'ui','build','index.html'));
     })
 }else{
     app.get('/',(req,res)=>{
